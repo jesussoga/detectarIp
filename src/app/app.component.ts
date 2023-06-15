80f4f8459be751577a47baaf12351f9d29d0ef37
jesussoga <em.sotoca@gmail.com>
2023-06-15 13:51:49 +0200

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {DireccionIp} from "./models/direccionip.model";
+import {Localizacion} from "./models/localizacionip.model";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejercicio147';
+  public direccionIp: DireccionIp;
+  public cargando: boolean;
+  public geoLocalizacion! : Localizacion;
+
+  constructor(private httpClient: HttpClient) {
+    this.direccionIp = {ip : ""};
+    this.cargando = false;
+  }
+
+  public obtenerIp(): void{
+    this.cargando = true;
+      this.httpClient.get<DireccionIp>("https://api.ipify.org/?format=json").subscribe(
+        {
+          next: (datos: DireccionIp)=> {
+            console.log("Ya han llegado los datos", datos);
+            this.direccionIp = datos;
+            this.cargando = false;
+          },
+          error: (error)=> {
+            console.log("No han llegado los datos", error);
+            this.cargando = false;
+          }
+        }
+      );
+
+  }public localizacion(): void{
+      // this.httpClient.get<Localizacion>("https://ipinfo.io/" + this.direccionIp.ip + "/geo").subscribe(
+      this.httpClient.get<Localizacion>("https://ipinfo.io/188.85.194.218/geo").subscribe(
+        {
+          next: (datos: Localizacion)=> {
+            console.log("Ya han llegado los datos", datos);
+            this.geoLocalizacion = datos;
+          },
+          error: (error)=> {
+            console.log("No han llegado los datos", error);
+          }
+        }
+      );
+  }
+
 }
---
+++ src/app/models/localizacionip.model.ts
@@ -0,0 +1,12 @@
+export interface Localizacion {
+  ip: string;
+  hostname: string;
+  city: string;
+  region: string;
+  country: string;
+  loc: string;
+  org: string;
+  postal: string;
+  timezone: string;
+  readme: string;
+}
